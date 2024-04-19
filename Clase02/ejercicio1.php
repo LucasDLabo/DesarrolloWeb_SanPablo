c33fc2486bce6c28886d6a8d7f44f11ebbb75d21
LucasDLabo <lucas.dellabo@gmail.com>
2024-04-18 22:01:32 -0300

    Se realizo el segundo ejercicio que busca encontrar los números pares

--- Clase02/ejercicio1.php
+++ Clase02/ejercicio1.php
@@ -5,4 +5,19 @@ echo "Ejercicio 1 <br>";
         echo $i;
         echo "<br>";
     }
+
+// Ejercicio 2 
+//Realiza un programa que pida al usuario un número y muestre por pantalla
+//todos los números pares desde 0 hasta ese número utilizando un bucle
+
+$numero = 10;
+$i = 0;
+echo "<br>Ejercicio 2<br>";
+echo "Hasta que número contar: $numero";
+while ($i <= $numero):
+    $resto = $i % 2;
+    $par = ($resto == 0) ? "<br>El número $i es par" : "";
+    echo $par;
+    $i++;
+endwhile;
 ?>
