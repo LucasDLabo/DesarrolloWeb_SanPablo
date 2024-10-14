c2d6c34a04b4edc6b44c158246658881f742096d
LucasDelLabo <121238683+LucasDLabo@users.noreply.github.com>
2024-10-14 18:23:16 -0300

    refactor: Config Datatable Tailwind 1.0

--- TrabajoFinal/Views/Alumnos/index.view.php
+++ TrabajoFinal/Views/Alumnos/index.view.php
@@ -1,117 +1,81 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>👨‍🎓 Alumnos </title>
-    <!-- DataTables CSS library -->
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css" />
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap4.min.css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <!-- Incluye DataTables CSS -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.tailwindcss.css">
 
-    <link rel="stylesheet" href="//stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <script src="//code.jquery.com/jquery-3.5.1.js"></script>
-    <!-- DataTables JS library -->
-    <script type="text/javascript" src="//cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
-    <!-- DataTables JBootstrap -->
-    <script type="text/javascript" src="//cdn.datatables.net/1.11.3/js/dataTables.bootstrap4.min.js"></script>
-    <style type="text/css">
-        .bs-example {
-            margin: 20px;
-        }
-    </style>
 </head>
-
 <body>
-    <div class="bs-example">
-        <div class="container d-flex justify-content-center">
-            <div class="col-md-11">
-                <a href="../Materias/indexMateria.php"><button>Materias📚</button></a>
-                <a href="../Profesores/indexProfesor.php"><button>Profesores👨‍🏫</button></a>
-                <div class="page-header clearfix">
-                    <h2 class="pull-left">Lista de Alumnos 👩‍🎓</h2>
-                </div>
-                <table id="listaUsuarios" class="table table-sm table-striped table-bordered" style="width:100%">
-                    <nav class="nav justify-content-center  ">
-                        <a class="btn btn-primary" href="create.php" aria-current="page">
-                            Crear alumno 📖
-                        </a>
-                    </nav>
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Nombre</th>
-                            <th>Apellido</th>
-                            <th>Fecha de Nacimiento</th>
-                            <th>Accion</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <!-- <tr>
-                                <td>1</td>
-                                <td>Nicolás</td>
-                                <td>Rotili</td>
-                                <td>24/06/1993</td>
-                            </tr> -->
-                        <?php
-                        // Primer Forma
-                        // foreach ($alumnos as $alumnos){
-                        //     echo "<tr>";
-                        //         echo "<td>" . $alumnos->id . "</td>";
-                        //         echo "<td>" . $alumnos->nombre . "</td>";
-                        //         echo "<td>" . $alumnos->apellido . "</td>";
-                        //         echo "<td>" . $alumnos->fecha_nacimiento . "</td>";
-                        //     echo "<tr>";
-                        // }
 
-                        //Segunda Forma 
-                        // "<?="  equivale a un <?php echo
-                        foreach ($alumnos as $alumno) { ?>
+<div class="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
+    
+    <h2 class="text-2xl font-bold mb-4">Lista de Alumnos 👩‍🎓</h2>
+    <table id="listaAlumno" class="table-auto w-full">
+        <nav>
+        <a href="../Materias/indexMateria.php"><button>Materias📚</button></a>
+        <a href="../Profesores/indexProfesor.php"><button>Profesores👨‍🏫</button></a>
+        </nav>
+        <div>
+            <a href="" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Crear alumno 📖
+            </a>
+        </div>
+        <thead>
+            <tr>
+                <th class="px-4 py-2">ID</th>
+                <th class="px-4 py-2">Nombre</th>
+                <th class="px-4 py-2">Apellido</th>
+                <th class="px-4 py-2">Fecha de Nacimiento</th>
+                <th class="px-4 py-2">Acción</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($alumnos as $alumno) { ?>
                             <tr>
-                                <td><?= $alumno->id; ?></td>
-                                <td><?= $alumno->nombre; ?></td>
-                                <td><?= $alumno->apellido; ?></td>
-                                <td><?= date('d/m/Y', strtotime($alumno->fecha_nacimiento));  ?></td>
-                                <td>
+                                <td class="border px-4 py-2"><?= $alumno->id; ?></td>
+                                <td class="border px-4 py-2"><?= $alumno->nombre; ?></td>
+                                <td class="border px-4 py-2"><?= $alumno->apellido; ?></td>
+                                <td class="border px-4 py-2"><?= date('d/m/Y', strtotime($alumno->fecha_nacimiento));  ?></td>
+                                <td class="border px-4 py-2">
                                     <div>
-                                        <a href="showSubject.php?id=<?= $alumno->id; ?>" class="btn btn-info ">Ver Materias📖</a>
-                                        <a href="eliminar.php?id=<?= $alumno->id; ?>" class="btn btn-danger">Eliminar❌</a>
-                                        <a href="editar.php?id=<?= $alumno->id; ?>" class="btn btn-info">Editar✍️</a>
+                                        <a href="showSubject.php?id=<?= $alumno->id; ?>" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Ver Materias📖</a>
+                                        <a href="eliminar.php?id=<?= $alumno->id; ?>" class="bg-red-800 hover:bg-red-900 text-white font-bold py-2 px-4 rounded">Eliminar❌</a>
+                                        <a href="editar.php?id=<?= $alumno->id; ?>" class="bg-yellow-600 hover:bg-yellow-900 text-white font-bold py-2 px-4 rounded"> Editar✍️</a>
                                     </div>
                                 </td>
                             </tr>
                         <?php }
 
                         ?>
+        </tbody>
+        <tfoot>
+            <tr>
+                <th class="px-4 py-2">ID</th>
+                <th class="px-4 py-2">Nombre</th>
+                <th class="px-4 py-2">Apellido</th>
+                <th class="px-4 py-2">Fecha de Nacimiento</th>
+                <th class="px-4 py-2">Acción</th>
+            </tr>     
+        </tfoot>
+    </table>
+</div>
 
-                    </tbody>
-                    <tfoot>
-                        <tr>
-                            <th>ID</th>
-                            <th>Nombre</th>
-                            <th>Apellido</th>
-                            <th>Fecha de Nacimiento</th>
-                            <th>Accion</th>
-                        </tr>
-                    </tfoot>
-                </table>
-            </div>
-        </div>
-    </div>
-</body>
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+
 <script>
     $(document).ready(function() {
-        $('#listaUsuarios').DataTable({
-            "columns": [
-                null,
-                null,
-                null,
-                null,
-                {"width": "40%"}
-            ]
+        $('#listaAlumno').DataTable({
+            // Add any customization options here
         });
     });
 </script>
 
+</body>
 </html>
