17e677c3fe048f2f1703f248288ee4763cc7bb45
Lucas Del Labo <121238683+LucasDLabo@users.noreply.github.com>
2024-11-04 01:10:02 -0300

    Implementado Nuevo Diseño y Final en los Index

--- TrabajoFinal/Views/Alumnos/index.view.php
+++ TrabajoFinal/Views/Alumnos/index.view.php
@@ -10,27 +10,25 @@
     <!-- Incluye DataTables CSS -->
     <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.tailwindcss.css">
     <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css" rel="stylesheet" />
-
-
 </head>
 
 
 <body class="min-h-screen flex flex-col">
 
     <header>
-        <nav class="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700 shadow">
+        <nav class="border-gray-200 bg-white shadow dark:border-gray-700 dark:bg-gray-900">
             <div class="max-w-screen flex flex-wrap items-center justify-between  p-4">
                 <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse ">
                     <!-- <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" /> -->
                     <span
-                        class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white hover:text-blue-500">Ir
-                        a la Vista
-                        General </span>
+                        class="self-center text-xl font-semibold whitespace-nowrap dark:text-white hover:text-blue-500">
+                        Ir a la Vista General
+                    </span>
                 </a>
                 <button data-collapse-toggle="navbar-dropdown" type="button"
                     class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                     aria-controls="navbar-dropdown" aria-expanded="false">
-                    <span class="sr-only">Open main menu</span>
+                    <span class="sr-only">Abrir menu principal</span>
                     <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                         viewBox="0 0 17 14">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -60,18 +58,21 @@
                                     aria-labelledby="dropdownLargeButton">
                                     <li>
                                         <a href="#"
-                                            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Nuevo
-                                            Alumno</a>
+                                            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                            Nuevo Alumno
+                                        </a>
                                     </li>
                                     <li>
                                         <a href="#"
-                                            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Nuevo
-                                            Profesor</a>
+                                            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                            Nuevo Profesor
+                                        </a>
                                     </li>
                                     <li>
                                         <a href=""
-                                            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Nueva
-                                            Materia</a>
+                                            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                            Nueva Materia
+                                        </a>
                                     </li>
                                 </ul>
                                 <div class="py-1">
@@ -97,58 +98,49 @@
                 </div>
             </div>
         </nav>
+
     </header>
 
     <main class="flex flex-grow gap-x-4 px-4">
 
-        <div class="flex flex-col flex-grow m-5 ">
+        <div class="m-5 flex flex-grow flex-col">
 
-            <div class="flex flex-grow w-auto">
-
+            <div class="flex w-auto flex-grow">
                 <div
-                    class="flex flex-col w-1/6 content-center items-center text-center font-medium text-gray-500 shadow-[1px_0_0px_rgba(0,0,0,0.2)] mr-3">
+                    class="mr-3 flex w-1/6 flex-col content-center items-center text-center font-medium text-gray-500 shadow-[1px_0_0px_rgba(0,0,0,0.2)]">
                     <h2 class="pt-3">Índice</h2>
                     <hr class="mb-4 mt-3 w-8/12">
-
-                    <span class="flex flex-col w-full items-center gap-4">
+                    <span class="flex w-full flex-col items-center gap-4">
                         <a href="#"
-                            class=" px-4 py-3 text-white bg-blue-700 rounded-lg active w-3/5 dark:bg-blue-600"
+                            class="active w-3/5 rounded-lg bg-blue-700 px-4 py-3 text-white dark:bg-blue-600"
                             aria-current="page">
                             Alumnos
                         </a>
 
-
-                        <a href="#"
-                            class="px-4 py-3 rounded-lg hover:text-gray-900 bg-gray-100 hover:bg-gray-100 w-3/5 ">
+                        <a href="../Profesores/index.php"
+                            class="w-3/5 rounded-lg bg-gray-100 px-4 py-3 hover:bg-gray-100 hover:text-gray-900">
                             Profesores
                         </a>
 
-
                         <a href="../Materias/index.php"
-                            class="px-4 py-3 rounded-lg hover:text-gray-900 bg-gray-100 hover:bg-gray-100 w-3/5">
+                            class="px-4 w-3/5 rounded-lg bg-gray-100 py-3 hover:bg-gray-100 hover:text-gray-900">
                             Materias
                         </a>
                     </span>
-
-
                 </div>
 
                 <div name="datatable" class="w-full px-5">
-
                     <div class="flex flex-row items-end justify-between">
-                        <h1 class=" font-bold lg:text-3xl ">👩‍🎓 Lista de Alumnos </h1>
-
+                        <h1 class="font-bold lg:text-3xl ">👩‍🎓 Lista de Alumnos </h1>
                         <a href="crear.php">
                             <button
-                                class="rounded bg-emerald-800 justify-end -translate-y-1 text-sm px-3 py-2 font-bold text-white shadow-[0px_6px_0px_0px_#1a380c] hover:bg-emerald-900 active:translate-y-[2px] active:shadow-[0px_3px_0px_0px_#1a380c]">
+                                class="-translate-y-1 justify-end rounded bg-emerald-800 px-3 py-2 text-sm font-bold text-white shadow-[0px_6px_0px_0px_#1a380c] hover:bg-emerald-900 active:translate-y-[2px] active:shadow-[0px_3px_0px_0px_#1a380c]">
                                 Crear Alumno
                             </button>
                         </a>
-
                     </div>
-
                     <hr class="my-3">
-                    <table id="listaAlumno" class="table-auto border-solid border-2 border-gray-400 rounded stripe">
+                    <table id="listaAlumno" class="stripe table-auto rounded border-2 border-solid border-gray-400">
 
                         <thead>
                             <tr>
@@ -160,13 +152,17 @@
                                 <th class=" w-44 text-xs">Acción</th>
                             </tr>
                         </thead>
+
                         <tbody>
+
                             <?php foreach ($alumnos as $alumno) { ?>
+
                             <tr>
                                 <td class="border text-center"><?= $alumno->id ?></td>
                                 <td class="border"><?= $alumno->nombre ?></td>
                                 <td class="border"><?= $alumno->apellido ?></td>
-                                <td class="border text-center"><?= date('d/m/Y', strtotime($alumno->fecha_nacimiento)) ?></td>
+                                <td class="border text-center">
+                                    <?= date('d/m/Y', strtotime($alumno->fecha_nacimiento)) ?></td>
                                 <td class="border text-center">
                                     <div class=" ">
                                         <a href="verMaterias.php?id=<?= $alumno->id ?> "
@@ -180,18 +176,21 @@
 
                                         <a href="editar.php?id=<?= $alumno->id ?>"
                                             title="Editar Alumno <?= $alumno->nombre . ' ' . $alumno->apellido ?>"
-                                            class=" w-20 bg-teal-600 hover:bg-teal-900 text-white text-center font-semibold py-1 rounded">
+                                            class="w-20 rounded bg-teal-600 py-1 text-center font-semibold text-white hover:bg-teal-900">
                                             Editar</a>
                                         <a href="eliminar.php?id=<?= $alumno->id ?>"
                                             onclick="return confirmar('<?= $alumno->nombre . ' ' . $alumno->apellido ?>')"
                                             title="Eliminar Alumno <?= $alumno->nombre . ' ' . $alumno->apellido ?>"
-                                            class=" w-20 bg-rose-700 hover:bg-rose-900 text-white text-center font-semibold py-1 rounded">Eliminar</a>
+                                            class="w-20 rounded bg-rose-700 py-1 text-center font-semibold text-white hover:bg-rose-900">Eliminar</a>
 
                                     </div>
                                 </td>
                             </tr>
+
                             <?php } ?>
+
                         </tbody>
+
                         <tfoot>
                             <tr>
                                 <th class="text-xs">ID</th>
@@ -202,9 +201,9 @@
                                 <th class="text-xs">Acción</th>
                             </tr>
                         </tfoot>
+
                     </table>
                 </div>
-
             </div>
 
         </div>
